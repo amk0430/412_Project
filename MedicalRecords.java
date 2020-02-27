90b0f5ea8951f42cfc243590fe893694cf834c05
amk0430 <49156781+amk0430@users.noreply.github.com>
2020-02-27 10:04:16 -0500

    Added code for ViewMedicalRecord use case & fixed MedicalRecords class

---
+++ MedicalRecords.java
@@ -0,0 +1,15 @@
+public class MedicalRecords
+{
+  private ArrayList<String> medicalRecords;
+
+  /**
+   * This is the default constructor for MedicalRecords class
+   */
+  public MedicalRecords()
+  {
+
+  }
+
+  
+
+}
