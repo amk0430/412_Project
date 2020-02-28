d440b7d4c46b3a4cd84154f8f69913c21db0e928
amk0430 <49156781+amk0430@users.noreply.github.com>
2020-02-28 15:21:38 -0500

    fixed updatemedicalrecords formating

--- UpdateMedicalRecords.java
+++ UpdateMedicalRecords.java
@@ -1,36 +1,46 @@
-public class update medical records(){
+public class UpdateMedicalRecords
+{
    private long PatientID
    private String GeneralRecords
    private String Immunizations
    private String Prescriptions
 
 
-public update medical records(){
+  public UpdateMedicalRecords()
+  {
 
-}
-public long getPatientID()
-{
-  return PatientID;
-}
-public MedicalRecords updateGeneralRecords(){
-  return GeneralRecords;
-}
-public MedicalRecords updateimmunizations(){
-  if (PatientID == PatientID)
-     {
-       //show prescriptions
-     }
-     else
-     {
-       //"Patient ID does not match, unauthorized access"
-     }
-      return Immunizations;
-}
-}
-public long Prescriptions(){
-  return Prescriptions();
-}
-public long TestResults(){
-  return TestResults();
-}
+  }
+
+  public long getPatientID()
+  {
+    return PatientID;
+  }
+
+  public MedicalRecords updateGeneralRecords()
+  {
+    return GeneralRecords;
+  }
+
+  public MedicalRecords updateimmunizations()
+  {
+    if (PatientID == PatientID)
+       {
+         //show prescriptions
+       }
+       else
+       {
+         //"Patient ID does not match, unauthorized access"
+       }
+        return Immunizations;
+  }
+  
+  public long Prescriptions()
+  {
+    return Prescriptions();
+  }
+
+  public long TestResults()
+  {
+    return TestResults();
+  }
 }
