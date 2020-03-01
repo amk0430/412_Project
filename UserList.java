8036d6e8b327bbe1bd0875a1d1a966d643a6d1ee
0820hu <55966926+0820hu@users.noreply.github.com>
2020-03-01 17:19:07 -0500

    Update UserList.java
    Move authentication method from UserList class to LoginCntl class.

--- UserList.java
+++ UserList.java
@@ -1,3 +1,5 @@
+package pkg412_alphacare;
+
 import java.util.ArrayList;
 
 public class UserList {
@@ -9,23 +11,6 @@ public class UserList {
      */
     public UserList(){
 
-    }
-
-    /**
-     * Verifies that username and password pair is in user list
-     * @param unameInput Sets the username to current user
-     * @param pwordInput Sets the password to current user
-     * @return A boolean if username and password match a user in the list
-     */
-    public boolean verifyLogin(String unameInput, String pwordInput){
-        for(User theUser : userList){
-            if( (theUser.getUsername().equals(unameInput)) &&
-                (theUser.getPassword().equals(pwordInput)) )
-            {
-                return true;
-            }
-        }
-        return false;
     }
 
     /**
