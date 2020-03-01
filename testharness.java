ccb06d294643b2c5408b77525bd304a7d73dcdc1
0820hu <55966926+0820hu@users.noreply.github.com>
2020-03-01 17:19:55 -0500

    creating tests

---
+++ testharness.java
@@ -0,0 +1,46 @@
+package pkg412_alphacare;
+
+import java.util.ArrayList;
+
+public class UserList {
+
+    private ArrayList<User> userList;
+
+    /**
+     * This is the default constructor for UserList class
+     */
+    public UserList(){
+
+    }
+
+    /**
+     * Confirms we can add user to user list - If no duplication, add new user.
+     * @param newUser Used to compare if user is already in user list
+     * @return A boolean if we added user to the list or not
+     */
+    public boolean addUser(User newUser){
+        for(User theUser : userList){
+            if(theUser.getUsername().equals(newUser.getUsername())){
+                return false;
+            }
+        }
+        userList.add(newUser);
+        return true;
+    }
+
+    /**
+     * Returns a user object based on username
+     * @param uname Finds the user associated with this username
+     * @return A user associated with the username
+     */
+    public User getUser(String uname){
+        for(User currentUser : userList){
+            if(currentUser.getUsername().equals(uname))
+            {
+                return currentUser;
+            }
+        }
+        return null;
+    }
+
+}
