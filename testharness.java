20a4addc25ffde49db9a99a50945488396896843
0820hu <55966926+0820hu@users.noreply.github.com>
2020-03-01 18:46:22 -0500

    Update testharness.java

--- testharness.java
+++ testharness.java
@@ -1,46 +1,84 @@
+
 package pkg412_alphacare;
 
-import java.util.ArrayList;
-
-public class UserList {
-
-    private ArrayList<User> userList;
-
-    /**
-     * This is the default constructor for UserList class
-     */
-    public UserList(){
-
-    }
-
-    /**
-     * Confirms we can add user to user list - If no duplication, add new user.
-     * @param newUser Used to compare if user is already in user list
-     * @return A boolean if we added user to the list or not
-     */
-    public boolean addUser(User newUser){
-        for(User theUser : userList){
-            if(theUser.getUsername().equals(newUser.getUsername())){
-                return false;
-            }
+public class testharness {
+    
+    UserList userData = new UserList();
+    
+    public void test() {
+        
+        /**
+         * Test if we can add user to user list.
+         */
+        userData.addUser(new User("123", "123"));
+        
+        /**
+         * Test if we can return a user object based on username.
+         */
+        String testUsername = "123";
+        
+        if ("123".equals(userData.getUser(testUsername).getUsername())) {
+            System.out.println("There is a user with username of " + testUsername);
         }
-        userList.add(newUser);
-        return true;
-    }
-
-    /**
-     * Returns a user object based on username
-     * @param uname Finds the user associated with this username
-     * @return A user associated with the username
-     */
-    public User getUser(String uname){
-        for(User currentUser : userList){
-            if(currentUser.getUsername().equals(uname))
-            {
-                return currentUser;
-            }
+        
+        else {
+            System.out.println("Error. No matching user with username.");
         }
-        return null;
+        
+        /**
+         * Test Login function.
+         */
+        
+        /**
+         * Test if the data format is correct in
+         * PrescriptionInfo
+         * GeneralRecordInfo
+         * ImmunizationInfo
+         * TestResultInfo
+         * classes.
+         */
+        PrescriptionInfo prescriptioninfo = new PrescriptionInfo();
+        if (prescriptioninfo.toString() != null) {
+            System.out.println("Data format correct.");
+        }
+        else {
+            System.out.println("Error. No default data format.");
+        }
+        GeneralRecordInfo generalrecordinfo = new GeneralRecordInfo();
+        if (generalrecordinfo.toString() != null) {
+            System.out.println("Data format correct.");
+        }
+        else {
+            System.out.println("Error. No default data format.");
+        }
+        ImmunizationInfo immunizationinfo = new ImmunizationInfo();
+        if (immunizationinfo.toString() != null) {
+            System.out.println("Data format correct.");
+        }
+        else {
+            System.out.println("Error. No default data format.");
+        }
+        TestResultInfo testresultinfo = new TestResultInfo();
+        if (testresultinfo.toString() != null) {
+            System.out.println("Data format correct.");
+        }
+        else {
+            System.out.println("Error. No default data format.");
+        }
+        
+        /**
+         * Then test if the MedicalRecordsList object in MedicalRecordsList class
+         * can add data from above classes to itself.
+         */
+        MedicalRecordsList medicalrecordslist = new MedicalRecordsList();
+        medicalrecordslist.addPrescriptionToList();
+        medicalrecordslist.addGeneralRecordToList();
+        medicalrecordslist.addImmunizationsToList();
+        medicalrecordslist.addTestResultsToList();
+        
+        /**
+         * 
+         */
     }
-
+    
 }
