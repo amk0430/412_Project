5ba06b3f2b75ee46e020251a862fa254554953b6
0820hu <55966926+0820hu@users.noreply.github.com>
2020-03-01 17:11:07 -0500

    Update GeneralRecordInfo.java

--- GeneralRecordInfo.java
+++ GeneralRecordInfo.java
@@ -1,9 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- /**
- *
- * @author Amber Kirk
- */
+package pkg412_alphacare;
+
 public class GeneralRecordInfo {
     private String patientName;
     private String date;
