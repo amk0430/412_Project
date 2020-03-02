18371b2cefe7293abf4c8d774a06898a771c324d
0820hu <55966926+0820hu@users.noreply.github.com>
2020-03-01 23:39:54 -0500

    Update UserList.java
    Just added encapsulation and modified a little the add User method.

--- UserList.java
+++ UserList.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public class UserList {
 
-    private ArrayList<User> userList;
-
+    private ArrayList<User> userList = new ArrayList<User>();
+    
     /**
      * This is the default constructor for UserList class
      */
@@ -18,15 +18,49 @@ public class UserList {
      * @param newUser Used to compare if user is already in user list
      * @return A boolean if we added user to the list or not
      */
-    public boolean addUser(User newUser){
-        for(User theUser : userList){
+    public ArrayList<User> addUser(User newUser){
+        for(User theUser : getUserList()){
             if(theUser.getUsername().equals(newUser.getUsername())){
-                return false;
+                return getUserList();
+            }
+            else {
+                getUserList().add(newUser);
             }
         }
-        userList.add(newUser);
-        return true;
+        getUserList().add(newUser);
+        return getUserList();
     }
+    
+    /**
+     * Returns a user object based on username
+     * @param uname Finds the user associated with this username
+     * @return A user associated with the username
+     */
+    public User getUser(String uname){
+        for(User currentUser : getUserList()){
+            if(currentUser.getUsername().equals(uname))
+            {
+                return currentUser;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * @return the userList
+     */
+    public ArrayList<User> getUserList() {
+        return userList;
+    }
+
+    /**
+     * @param userList the userList to set
+     */
+    public void setUserList(ArrayList<User> userList) {
+        this.userList = userList;
+    }
+}
+
 
     /**
      * Returns a user object based on username
