58271b4deb52336980bd58b1bdb1fe2918744dbb
amk0430 <49156781+amk0430@users.noreply.github.com>
2020-03-04 19:19:16 -0500

    Fixed appointment classes

--- AppointmentCntl.java
+++ AppointmentCntl.java
@@ -24,5 +24,18 @@ public class AppointmentCntl {
         return appointmentList.getAppointmentfromList().get(index);
     }
 
+    public void addNewAppointment() {
+        String[] newAppointment = {"May 5", "12:00pm-1:00pm", "Dr.John"};
+        appointmentList.getAppointmentfromList().add(new AppointmentInfo(newAppointment));
+    }
 
+
+    public void updateAppointment(String newDate, String newAppointment, String newPhysician, int startIndex)
+    {
+        AppointmentInfo appointement = appointmentList.getAppointmentfromList().get(startIndex);
+        appointement.setDate(newDate);
+        appointement.setAppointmentTime(newAppointment);
+        appointement.setPhysician(newPhysician);
+    }
+             
 }
--- AppointmentGUI.java
+++ AppointmentGUI.java
@@ -13,53 +13,111 @@ import javax.swing.JTextField;
  * @author Amber Kirk
  */
 public class AppointmentGUI extends JFrame {
+    private int startIndex;
     private final AppointmentCntl appointmentCntl;
 
-    private final JTextField appointmentSearch = new JTextField();
-    private final JTextField appointmentInfo = new JTextField();
+    private JTextField appointmentDateDisplay = new JTextField(115);
+    private JTextField appointmentTimeDisplay = new JTextField(15);
+    private JTextField appointmetPhysicianDisplay = new JTextField();
 
-    private int recordIndex;
-
-    private JPanel selectorPanel;
+    private JPanel displayPanel;
     private JPanel buttonPanel;
 
-    public AppointmentGUI(AppointmentCntl appointmentCntl, int startIndex)
+    public AppointmentGUI(AppointmentCntl appointmentCntl, int recordIndex)
     {
         this.appointmentCntl = appointmentCntl;
-        recordIndex = startIndex;
+        startIndex = recordIndex;
         display();
         setFieldView();
     }
 
     private void display()
     {
-        setTitle("View Appointments");
+        setTitle("Appointment Viewer");
         setSize(1000, 500);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        selectorPanel = new JPanel(new GridLayout(5, 5));
-
-        selectorPanel.add(new JLabel("Appointments"));
-        selectorPanel.add(appointmentSearch);
+        displayPanel = new JPanel(new GridLayout(5, 5));
+        displayPanel.add(new JLabel("Date"));
+        displayPanel.add(appointmentDateDisplay);
+        displayPanel.add(new JLabel("Time"));
+        displayPanel.add(appointmentTimeDisplay);
+        displayPanel.add(new JLabel("Physician"));
+        displayPanel.add(appointmetPhysicianDisplay);
 
         buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
 
+        JButton nextButton = new JButton("Next");
+        JButton newButton = new JButton("New");
+        JButton previousButton = new JButton("Previous");
+        JButton updateButton = new JButton("Update");
+        JButton exitButton = new JButton("Exit");
 
-        JButton appointmentButton = new JButton("Appointment");
-        appointmentButton.addActionListener(event -> appointmentCntl.getAppointmentList());
-        buttonPanel.add(appointmentButton);
+
+        nextButton.addActionListener(event -> showNextAnimal());
+        newButton.addActionListener(event -> appointmentCntl.addNewAppointment());
+        previousButton.addActionListener(event -> showPreviousAppointment());
+        updateButton.addActionListener(event -> updateAppointment());
+        exitButton.addActionListener(event -> exitApp());
+
+        buttonPanel.add(previousButton);
+        buttonPanel.add(nextButton);
+        buttonPanel.add(newButton);
+        buttonPanel.add(updateButton);
+        buttonPanel.add(exitButton);
 
         setContentPane(new JPanel(new BorderLayout()));
-        getContentPane().add(selectorPanel, BorderLayout.CENTER);
+        getContentPane().add(displayPanel, BorderLayout.CENTER);
         getContentPane().add(buttonPanel, BorderLayout.SOUTH);
     }
 
+    private void showNextAnimal() {
+        int listSize = appointmentCntl.getAppointmentList().getAppointmentfromList().size();
+        startIndex++;
+
+        if (listSize == startIndex)
+        {
+            startIndex = 0;
+        }
+        setFieldView();
+    }
+
+    private void showPreviousAppointment() {
+        startIndex--;
+        setFieldView();
+    }
+
     private void setFieldView() {
+        appointmentDateDisplay.setText(appointmentCntl.getAppointment(startIndex).getDate());
+        appointmentTimeDisplay.setText(appointmentCntl.getAppointment(startIndex).getAppointmentTime());
+        appointmetPhysicianDisplay.setText(appointmentCntl.getAppointment(startIndex).getPhysician());
 
-        appointmentSearch.setText(appointmentCntl.getAppointmentList().toString());
-        appointmentSearch.setText(appointmentCntl.getAppointment(recordIndex).toString());
+    }
 
+    private void refreshDisplay(int index) {
+        startIndex = index--;
+        setFieldView();
+    }
+
+    public void updateAppointment() {
+
+
+        appointmentDateDisplay.getText();
+        appointmentTimeDisplay.getText();
+        appointmetPhysicianDisplay.getText();
+
+        String newDate = appointmentDateDisplay.getText();
+        String newAppointment = appointmentTimeDisplay.getText();
+        String newPhysician = appointmetPhysicianDisplay.getText();
+
+
+        appointmentCntl.updateAppointment(newDate, newAppointment, newPhysician, startIndex);
+
+    }
+
+    public void exitApp() {
+        System.exit(0);
     }
 
 }
